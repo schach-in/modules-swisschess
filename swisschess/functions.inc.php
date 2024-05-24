67d5c728864c1986e0695b0b9e61eb6f92ad6a63
Gustaf Mossakowski <gustaf@koenige.org>
2024-05-24 14:01:54 +0200

    move mf_swisschess_check() as mf_swisschess_filematch() to functions

--- swisschess/functions.inc.php
+++ swisschess/functions.inc.php
@@ -25,3 +25,67 @@ function mf_swisschess_tournament_type($data) {
 	if ($data[$key] === 1) return 'team';
 	return 'single';
 }
+
+/**
+ * check, whether SWT file matches tournament
+ *
+ * @param array $event
+ * @param string $event
+ * @param array $data
+ * return bool (exit on error)
+ */ 
+function mf_swisschess_filematch($event, $data) {
+	if (mf_swisschess_tournament_type($data) === 'single' AND !wrap_setting('tournaments_type_single')) {
+		wrap_error(
+			'Turnier wurde als Mannschaftsturnier angelegt, die SWT-Datei ist aber für ein Einzelturnier!',
+			E_USER_ERROR
+		);
+	}
+	if (mf_swisschess_tournament_type($data) === 'team' AND !wrap_setting('tournaments_type_team')) {
+		wrap_error(
+			'Turnier wurde als Einzelturnier angelegt, die SWT-Datei ist aber für ein Mannschaftsturnier!',
+			E_USER_ERROR
+		);
+	}
+
+	// no further check possible if IDs in Swiss Chess must not be used
+	if (wrap_setting('swisschess_ignore_ids')) return true;
+	
+	if (mf_swisschess_tournament_type($data) === 'single') {
+		$sql = 'SELECT person_id FROM participations
+			LEFT JOIN persons USING (contact_id)
+			WHERE usergroup_id = %d AND event_id = %d';
+		$sql = sprintf($sql,
+			wrap_id('usergroups', 'spieler'), $event['event_id']
+		);
+		$db_ids = wrap_db_fetch($sql, '_dummy_', 'single value');
+		$check_data = 'Spieler';
+		$id_field_name = 'person_id';
+		$field_key = 2038;
+	} else {
+		$sql = 'SELECT team_id
+			FROM teams
+			WHERE event_id = %d
+			AND team_status = "Teilnehmer"
+			ORDER BY fremdschluessel';
+		$sql = sprintf($sql, $event['event_id']);
+		$db_ids = wrap_db_fetch($sql, '_dummy_', 'single value');
+		$check_data = 'Teams';
+		$id_field_name = 'team_id';
+		$field_key = 1046;
+	}
+	$swt_ids = [];
+	foreach ($data[$check_data] as $line) {
+		// geht nur, wenn team_id oder person_id gesetzt ist
+		if (empty($line[$field_key])) continue;
+		parse_str($line[$field_key], $fields);
+		if (empty($fields[$id_field_name])) continue;
+		$swt_ids[] = $fields[$id_field_name];
+	}
+	$diff = array_diff($db_ids, $swt_ids);
+	if ($db_ids AND $swt_ids AND $diff === $db_ids) {
+		// Kein Team in Datenbank passt zu Teams aus SWT
+		wrap_error(sprintf('SWT-Import: Turnierdatei passt nicht zum Turnier. Bitte lade die richtige Datei hoch! (%s)', $event['identifier']), E_USER_ERROR);
+	}
+	return true;
+}
--- zzbrick_make/swtimport.inc.php
+++ zzbrick_make/swtimport.inc.php
@@ -59,7 +59,7 @@ function mod_swisschess_make_swtimport($vars, $settings, $event) {
 	$tournament = $tournament['out'];
 	
 	// Check: richtiges Turnier?
-	mf_swisschess_check($event, $tournament);
+	mf_swisschess_filematch($event, $tournament);
 
 	if ($_SERVER['REQUEST_METHOD'] === 'POST')
 		$import = mod_swisschess_make_swtimport_import($event, $tournament);
@@ -171,70 +171,6 @@ function mod_swisschess_make_swtimport_import($event, $tournament) {
 		$import['importiert'][] = $importiert;
 	}
 	return $import;
-}
-
-/**
- * Check, ob SWT-Datei zu Turnier passt, falls nicht: Abbruch mit Fehlermeldung
- *
- * @param array $event
- * @param string $event
- * @param array $data
- * return bool (exit on error)
- */ 
-function mf_swisschess_check($event, $data) {
-	if (mf_swisschess_tournament_type($data) === 'single' AND !wrap_setting('tournaments_type_single')) {
-		wrap_error(
-			'Turnier wurde als Mannschaftsturnier angelegt, die SWT-Datei ist aber für ein Einzelturnier!',
-			E_USER_ERROR
-		);
-	}
-	if (mf_swisschess_tournament_type($data) === 'team' AND !wrap_setting('tournaments_type_team')) {
-		wrap_error(
-			'Turnier wurde als Einzelturnier angelegt, die SWT-Datei ist aber für ein Mannschaftsturnier!',
-			E_USER_ERROR
-		);
-	}
-
-	// no further check possible if IDs in Swiss Chess must not be used
-	if (wrap_setting('swisschess_ignore_ids')) return true;
-	
-	if (mf_swisschess_tournament_type($data) === 'single') {
-		$sql = 'SELECT person_id FROM participations
-			LEFT JOIN persons USING (contact_id)
-			WHERE usergroup_id = %d AND event_id = %d';
-		$sql = sprintf($sql,
-			wrap_id('usergroups', 'spieler'), $event['event_id']
-		);
-		$db_ids = wrap_db_fetch($sql, '_dummy_', 'single value');
-		$check_data = 'Spieler';
-		$id_field_name = 'person_id';
-		$field_key = 2038;
-	} else {
-		$sql = 'SELECT team_id
-			FROM teams
-			WHERE event_id = %d
-			AND team_status = "Teilnehmer"
-			ORDER BY fremdschluessel';
-		$sql = sprintf($sql, $event['event_id']);
-		$db_ids = wrap_db_fetch($sql, '_dummy_', 'single value');
-		$check_data = 'Teams';
-		$id_field_name = 'team_id';
-		$field_key = 1046;
-	}
-	$swt_ids = [];
-	foreach ($data[$check_data] as $line) {
-		// geht nur, wenn team_id oder person_id gesetzt ist
-		if (empty($line[$field_key])) continue;
-		parse_str($line[$field_key], $fields);
-		if (empty($fields[$id_field_name])) continue;
-		$swt_ids[] = $fields[$id_field_name];
-	}
-	$diff = array_diff($db_ids, $swt_ids);
-	if ($db_ids AND $swt_ids AND $diff === $db_ids) {
-		// Kein Team in Datenbank passt zu Teams aus SWT
-		wrap_error(sprintf('SWT-Import: Turnierdatei passt nicht zum Turnier. Bitte lade die richtige Datei hoch! (%s)', $event['identifier']), E_USER_ERROR);
-	}
-	return true;
 }
 
 /**
